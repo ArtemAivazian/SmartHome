24db6f253d95f1284d471df172475f67768c6d46
Aivazian, Artem <aivazart@fel.cvut.cz>
2023-12-23 15:31:35 +0100

    documentation dev

--- SemestralWork/src/main/java/cz/cvut/fel/omo/semesrtalWork/observer/devices/documentation/Documentation.java
+++ SemestralWork/src/main/java/cz/cvut/fel/omo/semesrtalWork/observer/devices/documentation/Documentation.java
@@ -3,10 +3,15 @@ package cz.cvut.fel.omo.semesrtalWork.observer.devices.documentation;
 import cz.cvut.fel.omo.semesrtalWork.observer.devices.ADevice;
 
 public class Documentation {
-    private ADevice device;
-    private double timeToFix;
+    private final ADevice device;
+    private final double timeToFix;
 
+
+    public Documentation(ADevice device, double timeToFix) {
+        this.device = device;
+        this.timeToFix = timeToFix;
+    }
     public void fixDevice(ADevice device) {
-
+        System.out.println("Reading manual");
     }
 }
