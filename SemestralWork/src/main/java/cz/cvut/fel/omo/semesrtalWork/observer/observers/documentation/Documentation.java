4bf865e53bcba69cdb08ca6469bef5b9f8e0b356
zozulill <Z0663865482z>
2023-12-23 13:16:32 +0100

    Add documantation

--- SemestralWork/src/main/java/cz/cvut/fel/omo/semesrtalWork/observer/observers/documentation/Documentation.java
+++ SemestralWork/src/main/java/cz/cvut/fel/omo/semesrtalWork/observer/observers/documentation/Documentation.java
@@ -1,12 +1,18 @@
 package cz.cvut.fel.omo.semesrtalWork.observer.observers.documentation;
 
 import cz.cvut.fel.omo.semesrtalWork.observer.observers.ADevice;
+import java.util.logging.Logger;
 
 public class Documentation {
-    private ADevice device;
-    private double timeToFix;
+    private final ADevice device;
+    private final double timeToFix;
 
+
+    public Documentation(ADevice device, double timeToFix) {
+        this.device = device;
+        this.timeToFix = timeToFix;
+    }
     public void fixDevice(ADevice device) {
-
+        System.out.println("Reading manual");
     }
 }
